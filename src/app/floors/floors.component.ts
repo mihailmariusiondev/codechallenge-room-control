2b84dfaf76c13347dff2f46eaaa070db837669ad
Marius Mihail Ion <marius.mihail@enzo.es>
2022-09-08 12:30:40 +0200

    styling + add room button

--- src/app/floors/floors.component.ts
+++ src/app/floors/floors.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatOptionSelectionChange } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FloorService } from '@app/@shared';
 import { Floor } from '@app/@shared/models/floor';
 import { finalize } from 'rxjs';
+// import { DialogBoxComponent } from './components/dialog-box/dialog-box.component';
 
 @Component({
   selector: 'app-floors',
@@ -18,7 +21,7 @@ export class FloorsComponent implements OnInit {
   };
   isLoading = false;
 
-  constructor(private floorService: FloorService) {}
+  constructor(private floorService: FloorService, private snackBar: MatSnackBar, public dialog: MatDialog) {}
 
   ngOnInit() {
     this.isLoading = true;
@@ -37,5 +40,16 @@ export class FloorsComponent implements OnInit {
   onFloorChange(floor: MatOptionSelectionChange) {
     this.selectedFloor = floor.source.value;
     console.log(this.selectedFloor);
+  }
+
+  openDialog() {
+    // const dialogRef = this.dialog.open(DialogBoxComponent, {
+    //   width: '250px',
+    //   data: { action: action, user: user }
+    // });
+    // dialogRef.afterClosed().subscribe(result => {
+    //   if (result) {
+    //   }
+    // });
   }
 }
