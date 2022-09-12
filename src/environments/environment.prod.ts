40439e93f7a600069ccf230293fd5af73333500c
Marius Mihail Ion <marius.mihail@enzo.es>
2022-09-12 18:28:09 +0200

    update production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ import { env } from './.env';
 export const environment = {
   production: true,
   version: env['npm_package_version'],
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: 'https://json-server-heroku-floors.herokuapp.com',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US'],
 };
