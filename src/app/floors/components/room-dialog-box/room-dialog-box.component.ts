62622fe370963e6f5b48a5e02488a05141deba63
Marius Mihail Ion <marius.mihail@enzo.es>
2022-09-12 15:30:46 +0200

    only update if room has changed + bugfix

--- src/app/floors/components/room-dialog-box/room-dialog-box.component.ts
+++ src/app/floors/components/room-dialog-box/room-dialog-box.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Floor, RoomActions } from '@app/@shared/models/floor';
 import { Room } from '@app/@shared/models/room';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-room-dialog-box',
@@ -27,7 +28,7 @@ export class RoomDialogBoxComponent {
 
     this.roomForm = new FormBuilder().group({
       id: new FormControl(this.room?.id || Date.now()),
-      floor_id: new FormControl(this.room?.floor_id),
+      floor_id: new FormControl(this.floor?.id),
       name: new FormControl(this.room?.name, [Validators.required, Validators.maxLength(25)]),
       maximum_capacity: new FormControl(this.room?.maximum_capacity, [Validators.required, Validators.max(99)]),
       occupancy: new FormControl(this.room?.occupancy, [Validators.required, Validators.max(99)]),
@@ -40,7 +41,14 @@ export class RoomDialogBoxComponent {
         this.dialogRef.close({ action: RoomActions.CREATE, room: this.roomForm.value as Room });
         break;
       case RoomActions.UPDATE:
-        this.dialogRef.close({ action: RoomActions.UPDATE, room: this.roomForm.value as Room });
+        const roomPrevNextEqual = _.isEqual(
+          _.omit(_.omit(this.room, ['id', 'floor_id'])),
+          _.omit(_.omit(this.roomForm.value as Room, ['id', 'floor_id']))
+        );
+
+        if (!roomPrevNextEqual) {
+          this.dialogRef.close({ action: RoomActions.UPDATE, room: this.roomForm.value as Room });
+        }
         break;
       case RoomActions.DELETE:
         this.dialogRef.close({ action: RoomActions.DELETE, room: this.room });
